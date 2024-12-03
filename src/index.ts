4e040fa151306d2e2a5006711a115d3a60250758
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-04 00:24:39 +0700

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config"
-import { v5 as uuidv5 } from 'uuid';
+import { v5 } from 'uuid';
 import WebSocketReconnect from "@javeoff/ws-reconnect";
 
 const WS_URL = process.env.PROVIDER_API_URL || 'ws://localhost:8080';
@@ -9,45 +9,53 @@ interface DataObject {
   key: string;
 }
 
-let wsConnection: WebSocketReconnect | null = null;
-let objectKeys: string[] = [];
-let valueTypes: string[] = [];
-let isFirstRun = true;
+export function createProcessData() {
+  let wsConnection: WebSocketReconnect | null = null;
+  let objectKeys: string[] = [];
+  let valueTypes: string[] = [];
+  let isFirstRun = true;
 
-const NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
+  const NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
 
-export default function processData(data: DataObject) {
-  if (!WS_URL) {
-    return;
+  return function ({ key, ...data }: DataObject) {
+    if (!WS_URL) {
+      return;
+    }
+
+    if (!key) {
+      throw new Error('Key is required');
+    }
+
+    const id = v5(key, NAMESPACE);
+
+    if (!id) {
+      throw new Error('Invalid id');
+    }
+
+    if (isFirstRun) {
+      objectKeys = ['id', ...Object.keys(data)];
+      valueTypes = ['string', ...objectKeys.map(k => typeof data[k])];
+
+      const queryParams = new URLSearchParams({
+        keys: objectKeys.join(','),
+        types: valueTypes.join(',')
+      }).toString();
+
+      wsConnection = new WebSocketReconnect(`${WS_URL}/ws?${queryParams}`);
+
+      wsConnection.on('open', () => {
+        const values = [id, ...Object.values(data)].join(',');
+        wsConnection?.send(values);
+      })
+
+      isFirstRun = false;
+    } else if (wsConnection) {
+      const values = [id, ...Object.values(data)].join(',');
+      wsConnection.send(values);
+    }
+
+    return id;
   }
-
-  if (!data.key) {
-    throw new Error('Key is required');
-  }
-
-  const id = uuidv5(data.key, NAMESPACE);
-
-  if (isFirstRun) {
-    objectKeys = ['id', ...Object.keys(data)];
-    valueTypes = ['string', ...objectKeys.map(key => typeof data[key])];
-
-    const queryParams = new URLSearchParams({
-      keys: objectKeys.join(','),
-      types: valueTypes.join(',')
-    }).toString();
-
-    wsConnection = new WebSocketReconnect(`${WS_URL}/ws?${queryParams}`);
-
-    wsConnection.on('open', () => {
-      const values = [id, ...objectKeys].map(key => data[key]).join(',');
-      wsConnection?.send(values);
-    })
-
-    isFirstRun = false;
-  } else if (wsConnection) {
-    const values = [id, ...objectKeys].map(key => data[key]).join(',');
-    wsConnection.send(values);
-  }
-
-  return id;
 }
+
+export default createProcessData();
